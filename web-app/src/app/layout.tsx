4dcc2b8a5cb7f79b0673fab3ca2683ca8543feba
Nitish kumar <kumarn66346@gmail.com>
2026-08-27 09:16:00 +0530

    Created hero section 1

--- web-app/src/app/layout.tsx
+++ web-app/src/app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const roboto = Roboto({
   subsets: ["latin"],
+  variable: "--font-roboto",
+  weight: ["400", "500", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const poppins = Poppins({
   subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
@@ -21,9 +23,11 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${roboto.variable} ${poppins.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        {children}
+      </body>
     </html>
   );
 }
---
+++ web-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Image from "next/image";
+import Hero1 from "../components/herosection/Hero1";
+
+export default function Home() { 
+  return (
+    <div className="">
+      <Hero1/>
+    </div>
+  );
+}
